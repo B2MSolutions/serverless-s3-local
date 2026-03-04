a0ba7aafbbd7c3abee13e32aa287a9e228f31c1b
michaelrowe <michael.rowe@b2m-solutions.com>
2026-03-04 08:30:37 +0000

    Updated dependencies to fix Dependabot alerts

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const S3rver = require("s3rver");
+const S3rver = require("@20minutes/s3rver");
 const fs = require("fs-extra"); // Using fs-extra to ensure destination directory exist
 const S3 = require("aws-sdk/clients/s3");
 const shell = require("shelljs");
